63963c4ab797dde4b375bde2e9ca3055cdc411b7
Dmitry Bufistov <dmitry@midokura.com>
2019-03-20 17:10:06 +0100

    Check config merge order
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- src/main/java/gamelogic/GameConfig.java
+++ src/main/java/gamelogic/GameConfig.java
@@ -3,8 +3,7 @@ package gamelogic;
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({ "file:backgammon.config",
-    "file:/etc/backgammon.config",
+@Config.Sources({ "file:${configFileName}",
     "classpath:gamelogic/backgammon.properties" })
 public interface GameConfig extends Config {
     int boardSize();
--- src/test/java/gamelogic/GameConfigTest.java
+++ src/test/java/gamelogic/GameConfigTest.java
@@ -3,7 +3,9 @@ package gamelogic;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import gamelogic.GameConfig;
+
+import java.io.File;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +14,19 @@ public class GameConfigTest {
     @Test
     @DisplayName("Default values are loaded from classpath props")
     public void test1() {
+        ConfigFactory.setProperty(
+            "configFileName", "backgammon.config");
         GameConfig config = ConfigFactory.create(GameConfig.class);
         assertEquals(24, config.boardSize());
+    }
+
+    @Test
+    @DisplayName("Config file takes precedence over classpath file")
+    public void test2() throws Exception {
+        File tempFile = File.createTempFile("testbackgammon-", ".properties");
+        Files.write(tempFile.toPath(), "boardSize=101".getBytes());
+        ConfigFactory.setProperty("configFileName", tempFile.getAbsolutePath());
+        GameConfig config = ConfigFactory.create(GameConfig.class);
+        assertEquals(101, config.boardSize());
     }
 }
